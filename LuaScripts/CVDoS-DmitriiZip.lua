1af5df1f70997a3fdfc7c871177a90051d2437cf
gochaism <gochaism@c88a1104-702e-11de-8585-3f51e31374b7>
2009-07-18 05:43:49 +0000

    add texts

---
+++ LuaScripts/CVDoS-DmitriiZip.lua
@@ -0,0 +1,16 @@
+-- Dmitrii room zipping help (quite sloppy)
+
+if not emu then
+  error("This script runs under DeSmuME.")
+end
+
+emu.registerbefore(function()
+  local x = math.floor(memory.readword(0x020CA95E) / 0x10)
+  local y = math.floor(memory.readword(0x020CA962) / 0x10 % 192)
+  local i = (y * 16) + x + 1
+  local pos = 0x20F7085 + math.floor(i / 8)
+  local mask = math.pow(2, math.floor(i % 8))
+  gui.text(144, 4, string.format("%08X:%02x\n[%04X-%04X]", pos, mask,
+           0x7085 + math.floor(((0 * 16) + x + 1) / 8),
+           0x7085 + math.floor(((191 * 16) + x + 1) / 8)))
+end)
