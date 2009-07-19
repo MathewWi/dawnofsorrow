64bb1033860c82e073bbcd9a1dbeb829ed1caf25
gochaism <gochaism@c88a1104-702e-11de-8585-3f51e31374b7>
2009-07-19 14:46:47 +0000

    Memory addresses; I started writing a rough whole movie by the way.

--- LuaScripts/CVDoS-DmitriiZip.lua
+++ LuaScripts/CVDoS-DmitriiZip.lua
@@ -1,4 +1,4 @@
--- Dmitrii room zipping help (quite sloppy)
+-- Dmitrii room zipping help (nah, it's wrong. I don't know the real formula :/)
 
 if not emu then
   error("This script runs under DeSmuME.")
@@ -11,6 +11,6 @@ emu.registerbefore(function()
   local pos = 0x20F7085 + math.floor(i / 8)
   local mask = math.pow(2, math.floor(i % 8))
   gui.text(144, 4, string.format("%08X:%02x\n[%04X-%04X]", pos, mask,
-           0x7085 + math.floor(((0 * 16) + x + 1) / 8),
-           0x7085 + math.floor(((191 * 16) + x + 1) / 8)))
+           0x7085 - 0x198 + math.floor(((0 * 16) + x + 1) / 8),
+           0x7085 - 0x118 + math.floor(((191 * 16) + x + 1) / 8)))
 end)
