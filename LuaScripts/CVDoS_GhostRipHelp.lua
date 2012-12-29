c6d3fb0950413b9c1947d0a3e403922709693da7
gochaism <gochaism@c88a1104-702e-11de-8585-3f51e31374b7>
2012-12-29 01:51:31 +0000

    GhostReplay: implemented Julius Mode

--- LuaScripts/CVDoS_GhostRipHelp.lua
+++ LuaScripts/CVDoS_GhostRipHelp.lua
@@ -4,7 +4,8 @@ if not bit then require("bit") end
 
 root = ""
 sprw, sprh, sprox, sproy = 128, 128, 64, 100
-chgf = "somadb.png"
+chgf = "julidb.png"
+screenshotKey = "F11"
 
 -- return if an image is a truecolor one
 gd.isTrueColor = function(im)
@@ -35,7 +36,7 @@ gd.convertToTrueColor = function(imsrc)
 	im:alphaBlending(false)
 	local trans = im:colorAllocateAlpha(255, 255, 255, 127)
 	im:filledRectangle(0, 0, im:sizeX() - 1, im:sizeY() - 1, trans)
-	im:copy(imsrc, 0, 0, 0, 0, im:sizeX(), im:sizeY())
+	im:copyResized(imsrc, 0, 0, 0, 0, im:sizeX(), im:sizeY(), im:sizeX(), im:sizeY())
 	im:alphaBlending(true) -- TODO: set the mode which imsrc uses
 
 	return im
@@ -74,9 +75,71 @@ gd.flipBoth = function(im)
 	gd.flipHorizontal(im)
 	return im
 end
+-- compare two images and extract only different pixels between two images
+-- return value is a truecolor gd image with alpha channel
+gd.createDiff = function(imFG, imBG, ...)
+	local arg = { ... }
+	local xOffsetFG, yOffsetFG = 0, 0
+	local xOffsetBG, yOffsetBG = 0, 0
+	local width, height = math.min(imFG:sizeX(), imBG:sizeX()), math.min(imFG:sizeY(), imBG:sizeY())
 
-chgl = gd.createFromPng(root..chgf):gdStr()
-chgr = gd.flipHorizontal(gd.createFromPng(root..chgf)):gdStr()
+	-- parse argument array
+	if #arg == 4 then
+		-- xOffset, yOffset, width, height
+		xOffsetFG, yOffsetFG = arg[1], arg[2]
+		xOffsetBG, yOffsetBG = xOffsetFG, yOffsetFG
+		width, height = arg[3], arg[4]
+	elseif #arg == 6 then
+		-- xOffsetFG, yOffsetFG, xOffsetBG, yOffsetBG, width, height
+		xOffsetFG, yOffsetFG = arg[1], arg[2]
+		xOffsetBG, yOffsetBG = arg[3], arg[4]
+		width, height = arg[5], arg[6]
+	elseif #arg > 0 then
+		error("too few/much arguments")
+	end
+
+	-- range check
+--	if (xOffsetFG + width > imFG:sizeX()) or (yOffsetFG + height > imFG:sizeY()) then
+--		error("foreground image (argument #1) is too small, or illegal offset. " .. width .. "x" .. height .. " from (" .. xOffsetFG .. "," .. yOffsetFG .. ")")
+--	end
+--	if (xOffsetBG + width > imBG:sizeX()) or (yOffsetBG + height > imBG:sizeY()) then
+--		error("background image (argument #2) is too small, or illegal offset. " .. width .. "x" .. height .. " from (" .. xOffsetBG .. "," .. yOffsetBG .. ")")
+--	end
+
+	-- create new gd image
+	local imDiff = gd.createTrueColorBlank(width, height)
+	imDiff:alphaBlending(false)
+	imDiff:copyResized(imFG, 0, 0, xOffsetFG, yOffsetFG, width, height, width, height)
+
+	-- pixel-by-pixel processing
+	local colTrans = imDiff:colorAllocateAlpha(255, 255, 255, 127)
+	for y = 0, height - 1 do
+		for x = 0, width - 1 do
+			local colFG = imFG:getPixel(x + xOffsetFG, y + yOffsetFG)
+			local colBG = imBG:getPixel(x + xOffsetBG, y + yOffsetBG)
+			if colFG and colBG then
+				if imFG:red(colFG) == imBG:red(colBG) and
+				   imFG:green(colFG) == imBG:green(colBG) and
+				   imFG:blue(colFG) == imBG:blue(colBG)
+				then
+					imDiff:setPixel(x, y, colTrans)
+				end
+			end
+		end
+	end
+	imDiff:alphaBlending(true)
+	imDiff:saveAlpha(false)
+	return imDiff
+end
+
+gdL = gd.convertToTrueColor(gd.createFromPng(root..chgf))
+gdR = gd.flipHorizontal(gd.convertToTrueColor(gd.createFromPng(root..chgf)))
+gdL:alphaBlending(true)
+gdR:alphaBlending(true)
+gdL:saveAlpha(true)
+gdR:saveAlpha(true)
+chgl = gdL:gdStr()
+chgr = gdR:gdStr()
 
 function chDrawSprite(x, y, n, reverse)
 	local xi, yi = (n % 0x10), math.floor(n / 0x10)
@@ -87,7 +150,10 @@ function chDrawSprite(x, y, n, reverse)
 	end
 end
 
+local input_saved = input.get()
+local image_save_lock = false
 gui.register(function()
+	local input_curr = input.get()
 	-- ch_visible = bit.band(memory.readbyte(0x020fcc55), 0x80)==0
 	room_x = memory.readbyte(0x020f6e20)
 	room_y = memory.readbyte(0x020f6e22)
@@ -122,6 +188,29 @@ gui.register(function()
 	for i = 0x020df77c, 0x020df85c, 0x10 do
 		memory.writedword(i, 0x00000000)
 	end
+
+	if input_curr[screenshotKey] and not input_saved[screenshotKey] and not image_save_lock then
+		image_save_lock = true
+
+		local gdEmu = gd.createFromPngStr(gd.createFromGdStr(gui.gdscreenshot()):pngStr())
+		--gdEmu:png(root.."screenshot.png")
+		local gdBG = gd.createFromPng(root.."SpriteRipBG.png")
+		local x, y = ch_x - camx - sprox, ch_y - camy - sproy + 192
+		local gdDiff = gd.createDiff(gdEmu, gdBG, x, y, sprw, sprh)
+		local xOfsDB, yOfsDB = (ch_spr%0x10) * sprw, math.floor(ch_spr/0x10) * sprh
+		gdL:alphaBlending(false)
+		gdL:copy(gdDiff, xOfsDB, yOfsDB, 0, 0, sprw, sprh)
+		gdL:alphaBlending(true)
+		gdL:png(root..chgf)
+		print(chgf .. ": sprite #" .. ch_spr .. " saved")
+
+		-- update current display
+		chgl = gdL:gdStr()
+
+		image_save_lock = false
+	end
+
+	input_saved = input_curr
 
 	gui.opacity(1 * fade)
 	gui.box(ch_x - camx - sprox, ch_y - camy - sproy, ch_x - camx - sprox + sprw - 1, ch_y - camy - sproy + sprh - 1, "clear", "#ff000080")
